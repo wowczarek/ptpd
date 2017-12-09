06a2a00dbcbc4f57350fe905b07c185554d2d5fd
Costin Popescu <costin.popescu@enea.com>
2017-12-09 08:46:16 +0100

    Fixed startup crashes caused by missing arguments in CCK log messages and consumed variable argument list in logMessageWrapper.

--- src/libcck/ttransport/socket_udpv4.c
+++ src/libcck/ttransport/socket_udpv4.c
@@ -108,7 +108,8 @@ tTransport_init(TTransport* self, const TTransportConfig *config, CckFdSet *fdSe
     copyCckTransportAddress(&self->hwAddress, &myData->intInfo.hwAddress);
 
     if(!self->hwAddress.populated) {
-	CCK_NOTICE(THIS_COMPONENT"tTransportInit(%s): No hardware address available, using protocol address for EUID\n");
+	CCK_NOTICE(THIS_COMPONENT"tTransportInit(%s): No hardware address available, using protocol address for EUID\n",
+		self->name);
     }
 
     /* if initialising with existing config, it's a restart */
